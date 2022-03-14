908e6ea35cf2d84258f7d91691ca535faa3fa1f7
Alexandru Gherasie <alexandru.gherasie@epitech.eu>
2022-03-14 15:10:00 +0100

    [ADD] structs for keys and game

--- include/tetris.h
+++ include/tetris.h
@@ -7,11 +7,33 @@
 
 #ifndef TETRIS_H_
     #define TETRIS_H_
-
     #include <stdlib.h>
+    #include <stdio.h>
     #include <ncurses.h>
     #include <sys/stat.h>
     #include <fcntl.h>
     #include "my.h"
+
+typedef struct vector2 {
+    int x;
+    int y;
+} vector2_t;
+
+typedef struct keys {
+    int l;
+    int r;
+    int t;
+    int d;
+    int q;
+    int p;
+} keys_t;
+
+typedef struct game {
+    int level;
+    keys_t keys;
+    vector2_t map_size;
+    int show_next;
+    int debug_mode;
+} game_t;
 
 #endif /* TETRIS_H_ */

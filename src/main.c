e06bc44aca145d3d45c55a92c781d10b5072be78
Alexandru Gherasie <alexandru.gherasie@epitech.eu>
2022-03-14 15:30:43 +0100

    [ADD] init params

--- src/main.c
+++ src/main.c
@@ -7,6 +7,30 @@
 
 #include "../include/tetris.h"
 
+keys_t *init_keys()
+{
+    keys_t *keys = malloc(sizeof(keys_t));
+    keys->l = KEY_LEFT;
+    keys->r = KEY_RIGHT;
+    keys->t = KEY_UP;
+    keys->d = KEY_DOWN;
+    keys->q = 'q';
+    keys->p = ' ';
+    return keys;
+}
+
+game_t *init_params()
+{
+    game_t *game = malloc(sizeof(game_t));
+    game->debug_mode = FALSE;
+    game->keys = init_keys();
+    game->map_size = (vector2_t) {10, 20};
+    game->level = 1;
+    game->debug_mode = FALSE;
+    game->show_next = FALSE;
+    return game;
+}
+
 int main(int ac, char **av)
 {
     for (int i = 1; av[i]; i++)
@@ -14,5 +38,11 @@ int main(int ac, char **av)
             printf("%s\n", read_to_charstar("help.txt"));
             return 0;
         }
+    initscr();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+    start_color();
+    init_pair(0, COLOR_WHITE, COLOR_BLACK);
+    game_t *game = init_params();
     return 0;
 }
